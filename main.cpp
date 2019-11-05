a43c54f9d4ad430934a0d00da0964a164573529d
Milad Doorbash <milad.doorbash@gmail.com>
2019-11-05 07:49:25 +0330

    1st commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <QtWidgets>
+
+// Include header files for application components.
+// ...
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    QMainWindow mainWin;
+    mainWin.show();
+    return app.exec();
+}
